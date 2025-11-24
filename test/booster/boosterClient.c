7113214e805a590d9ba47bd988c0e1041acf915e
Kaitlyn-Zeigler <kaitlyn@DESKTOP-90NHH1T.localdomain>
2025-11-23 20:54:12 -0500

    add performance metrics

--- test/booster/boosterClient.c
+++ test/booster/boosterClient.c
@@ -5,17 +5,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
 
 int main(int argc, char**argv){
 	Matrix firstA;
 	double lambda = 1;
 	int max_depth_param = 6;
+	struct timespec startT, endT;
 
 	printf("Tests contained in boosterADT.c:\n\n");
 
 	if (argc>=2){
 		firstA = create_from_file(argv[1]);
-	} else {printf("no file??\n"); return 1;}
+	} else {printf("no file?? \n"); return 1;}
 	
 	double train_size=0.8;
 	if (argc==3){
@@ -35,8 +37,10 @@ int main(int argc, char**argv){
 	my_parameters.gamma=1;		 //this one has not been implemented yet
 
 	Booster b = create_booster(trainM, my_parameters); //load the booster
+	clock_gettime(CLOCK_MONOTONIC, &startT);
 	train_booster(b); //train it--make the trees.  This could be wrapped in the create_booster if we would like to do that
-	
+	clock_gettime(CLOCK_MONOTONIC, &endT);
+
 	//verify the training dataset accuracy.  
 	printf("Check the training observations\n");
 	int total_correct=0;
@@ -55,6 +59,10 @@ int main(int argc, char**argv){
 	//verify the testing dataset accuracy
 	printf("\n\nCheck the testing observations\n");
 	total_correct=0;
+	int tn = 0;
+	int tp = 0;
+	int fn = 0;
+	int fp = 0;
 	for (int i=0; i<get_num_obs(testM); i++){
 		printf("Test obs %d.  Weight %lf Pred label %d True label %d\n", 
 			i,
@@ -62,7 +70,19 @@ int main(int argc, char**argv){
 			pred=get_predicted_label(b, testM, i),
 			actual=get_label(testM, i)
 		      );
-		if (pred==actual) total_correct++;
+		if (pred==actual) {
+			total_correct++;
+			if (pred==0) tn++;
+			if (pred==1) tp++;
+		} else {
+			if (pred==0) fn++;
+			if (pred==1) fp++;
+		}
 	}
 	printf("\nTest Accuracy:  %d/%d = %lf\n", total_correct, get_num_obs(testM), (double)total_correct/get_num_obs(testM));
+	printf("\nTest Sensitivity: %d/(%d+%d) = %lf\n", tp, tp, fn, (double)tp/(tp+fn));
+	printf("\nTest Specificity: %d/(%d+%d) = %lf\n", tn, tn, fp, (double)tn/(tn+fp));
+
+	double totalTime = (endT.tv_sec-startT.tv_sec)+((endT.tv_nsec-startT.tv_nsec)/1000000000.0);
+        printf("Training Time: %f\n", totalTime);
 }
